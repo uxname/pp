3513f34a1497cd3c918812e5de72b154d85e40ac
uxname <uxname@gmail.com>
2026-01-24 16:36:56 +0300

    Improve debug command log message for clarity

--- src/debug/debug.command.ts
+++ src/debug/debug.command.ts
@@ -3,6 +3,6 @@ import { Command, CommandRunner } from 'nest-commander';
 @Command({ name: 'debug', description: 'Debug command' })
 export class DebugCommand extends CommandRunner {
   async run(inputs: string[], options: Record<string, unknown>) {
-    console.log({ inputs, options });
+    console.log('Debugging command executed with:', { inputs, options });
   }
 }
